5b92f53e58452ce1ad0898dd5325fad6ecdea938
Waiyin Lau <yuyinja@gmail.com>
2019-12-08 23:04:36 -0500

    day 3 part 1 solution added

--- advent2019/day03/solution.py
+++ advent2019/day03/solution.py
@@ -17,25 +17,47 @@ PATH_PROCESS = {"L": (-1, 0), "R": (1, 0), "U": (0, 1), "D": (0, -1)}
 
 class PathObject(object):
     def __init__(self, inputdata):
+        self.inputs = inputdata
         self.units = [(0, 0)]
         self.currentPos = (0, 0)
-        self.move(inputdata)
+        self.move()
 
-    def move(self, inputs):
-        for input in inputs:
+    def move(self):
+        for input in self.inputs:
             dir = np.array(PATH_PROCESS[input[0]])
             units = int(input[1:])
             moved = (dir * units)
             self.currentPos += moved
             self.units.append(tuple(self.currentPos))
 
+    def lengthToIntersection(self, intersection):
+        pos = (0,0)
+        length = 0
+        for input in self.inputs:
+            dir = np.array(PATH_PROCESS[input[0]])
+            units = int(input[1:])
+            unitsCovering = [tuple((dir * i) + pos) for i in range(units)]
+            if intersection in unitsCovering:
+                add = unitsCovering.index(intersection)
+                length += add
+                return length
+            moved = (dir * units)
+            pos += moved
+            length += units
 
 pathA = PathObject(pathAdata)
 pathB = PathObject(pathBdata)
 
 lineA = LineString(pathA.units)
 lineB = LineString(pathB.units)
-
 intersections = lineA.intersection(lineB)
-intersections = sorted(intersections, key=lambda x: (abs(x.x) + abs(x.y)))
-print abs(intersections[1].x) + abs(intersections[1].y)
+
+def partA(intersections):
+    intersections = sorted(intersections, key=lambda x: (abs(x.x) + abs(x.y)))
+    return (abs(intersections[1].x) + abs(intersections[1].y))
+
+def partB(intersections):
+    intersections = [tuple((x.x, x.y)) for x in intersections]
+    intersections = sorted(intersections, key=lambda intersection:(pathB.lengthToIntersection(intersection) + pathA.lengthToIntersection(intersection)))
+
+    return (pathB.lengthToIntersection(intersections[1])+ pathA.lengthToIntersection(intersections[1]))
